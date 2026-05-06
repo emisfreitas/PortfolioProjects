243ea6e3210308e96dbd596ca237538561698696
emisfreitas <emilymoraes526@gmail.com>
2026-05-06 05:36:39 -0300

    Removendo pasta nft-price-prediction

--- nft-price-prediction/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_KEY = os.getenv("MORALIS_API_KEY")
-BASE_URL = "https://deep-index.moralis.io/api/v2.2"
-HEADERS = {"X-API-Key": API_KEY}
-CHAIN = "eth"
--- nft-price-prediction/debug_metadata.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("trades_enriched.csv")
-
-print("=== SAMPLE CAMPO METADATA ===")
-print(df["metadata"].iloc[0])
-print("\n---")
-print(df["metadata"].iloc[1])
